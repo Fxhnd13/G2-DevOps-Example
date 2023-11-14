0d54f496c0efaa91e3e0c4a6e08ab2a6c46d8aab
Soberanis <jcsru13@gmail.com>
2023-11-14 14:58:37 -0600

    Update: Actualización de lenguaje

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hola mundo!!!'));
 
 app.listen(port, () =>{
     console.log('La aplicación está escuchando en http://localhost:3000');
