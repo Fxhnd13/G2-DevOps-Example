f993607bdb9089fb26440fa03083e6becb13e58b
Soberanis <jcsru13@gmail.com>
2023-11-16 21:23:48 -0600

    Update: Mensaje de saludo actualizado

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hola mundo wuuu!!!'));
+app.get('/', (req, res) => res.send('Hola estudiantes de Gerenciales 2!!!'));
 
 app.listen(port, () =>{
     console.log('La aplicación está escuchando en http://localhost:3000');
